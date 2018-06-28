ea6c7fae2d309202b3d791dc6f489acec2725b4a
Damian Ziobro <devdamianziobro@gmail.com>
2018-06-28 21:04:11 +0200

    Updated App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import EthereumClassicOrders from './containers/EthereumClassicOrders/EthereumClassicOrders';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <EthereumClassicOrders />
       </div>
     );
   }
